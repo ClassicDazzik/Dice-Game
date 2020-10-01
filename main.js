ce8d5da77056926f1cdd80cf31463b0baad7c64a
Tuomas Puro <tuomas.puro@esedu.fi>
2020-10-01 08:43:45 +0300

    Vähän ideoita

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+// Muuttujat
+let roundScore = 0; // jemmataan heittokierroksen pisteet
+let turn = 0;
+
+const players = [
+    {
+        name: "Player1",
+        points: 0
+    },
+    {
+        name: "Player2",
+        points: 0
+    }
+];
+
 function rolldice() {
-    document.getElementById("scoretest").innerHTML =
-    Math.floor(Math.random() * 6) + 1;
+    let dice = Math.floor(Math.random() * 6) + 1;
+    roundScore = roundScore + dice;
+    document.getElementById("scoretest").innerHTML = dice;
+    return dice;
 }
 
 console.log(rolldice)
