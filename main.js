9449eb41c43d997e3de681f1a749a07121de0b4e
Clazzic <nikairik@gmail.com>
2020-10-13 07:57:16 +0300

    Improved css and made further scripting

--- main.js
+++ main.js
@@ -1,23 +1,59 @@
-// Muuttujat
-let roundScore = 0; // jemmataan heittokierroksen pisteet
+// Variables
+let roundScore = 0; 
 let turn = 0;
+var plr1score = 0;
+var plr2score = 0;
 
-const players = [
+let players = [
     {
         name: "Player1",
-        points: 0
     },
     {
         name: "Player2",
-        points: 0
     }
 ];
 
+// Rolls the dice.
+
 function rolldice() {
+    var currentPlayer = players[turn];
     let dice = Math.floor(Math.random() * 6) + 1;
     roundScore = roundScore + dice;
+    if(dice == 1) {
+        changeTurn()
+    }
+    document.getElementById("roundScore").innerHTML = roundScore;
     document.getElementById("scoretest").innerHTML = dice;
     return dice;
 }
 
-console.log(rolldice)
+// Changes turn to another player and resets the round score.
+
+function changeTurn() {
+    turn++;
+    if(turn == players.length) {
+       turn = 0;}
+    roundScore = 0;
+    document.getElementById("playerturn").innerHTML = players[turn].name;
+}
+
+// Adds a score to a player, then changes the turn.
+function calcScore() {
+    if(turn == 0) {
+        plr1score = plr1score + roundScore;
+    }
+    if(turn == 1) {
+        plr2score = plr2score + roundScore;
+    }
+    changeTurn();
+    document.getElementById("p1score").innerHTML = plr1score;
+    document.getElementById("p2score").innerHTML = plr2score;
+}
+
+// Ends the game by reloading the page when either players plrscore goes to 100.
+
+function endGame() {
+    if(plr1score || plr2score == 100) {
+    location.reload();
+    }
+}
