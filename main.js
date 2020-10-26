ad172707d03f4979094724e983dc337dc55a7f91
Clazzic <nikairik@gmail.com>
2020-10-26 21:40:27 +0200

    Improved coding

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // Variables
 let roundScore = 0; 
 let turn = 0;
-var plr1score = 0;
-var plr2score = 0;
+let endscore = 20
 
 let players = [
     {
         name: "Player1",
+        score: 0
     },
     {
         name: "Player2",
+        score: 0
     }
 ];
 
@@ -38,22 +39,20 @@ function changeTurn() {
 }
 
 // Adds a score to a player, then changes the turn.
+
 function calcScore() {
-    if(turn == 0) {
-        plr1score = plr1score + roundScore;
-    }
-    if(turn == 1) {
-        plr2score = plr2score + roundScore;
-    }
+    players[turn].score += roundScore;
+    document.getElementById("p1score").innerHTML = players[0].score;
+    document.getElementById("p2score").innerHTML = players[1].score;
+    endGame();
     changeTurn();
-    document.getElementById("p1score").innerHTML = plr1score;
-    document.getElementById("p2score").innerHTML = plr2score;
 }
 
-// Ends the game by reloading the page when either players plrscore goes to 100.
+// Ends the game by reloading the page when either player's score goes to 100.
 
 function endGame() {
-    if(plr1score || plr2score == 100) {
-    location.reload();
+    if(players[turn].score > endscore) {
+        alert(players[turn].name + " won!");
+        console.log("plr win");
     }
 }
